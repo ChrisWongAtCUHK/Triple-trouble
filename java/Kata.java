d4a79abb2a3c75afe447c5906a86b61bb27ce71c
chriswongatcuhk <wc38.wong@gmail.com>
2017-03-06 06:19:45 +0800

    My Java solution.

---
+++ java/Kata.java
@@ -0,0 +1,16 @@
+public class Kata {
+	public static int TripleDouble(long num1, long num2) {
+		String string1 = String.valueOf(num1);
+		String string2 = String.valueOf(num2);
+
+		for(int i = 0; i < string1.length(); i++){
+			char c = string1.charAt(i);
+			if(string1.contains(new String(new char[]{c, c, c})) &&
+					string2.contains(new String(new char[]{c, c}))){
+				return 1;
+			}
+		}
+
+		return 0;
+	}
+}
